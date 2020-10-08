5c7c340550461b434f9ebe971458f0353495378e
ls <ls@qq.com>
2020-10-08 18:53:19 +0800

    修改git-test

--- git01/src/main/java/Hello.java
+++ git01/src/main/java/Hello.java
@@ -5,5 +5,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!----git-test");
+        System.out.println("Hello World!----git-test");
+        System.out.println("Hello World!----git-test");
     }
 }
