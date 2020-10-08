3805b0ec03da0cc38525f8f60f4e955410a381eb
ls <ls@qq.com>
2020-10-08 19:41:45 +0800

    sw01修改git-test\

--- git01/src/main/java/Hello.java
+++ git01/src/main/java/Hello.java
@@ -10,7 +10,5 @@ public class Hello {
         System.out.println("Hello World!----git-test02");
         System.out.println("Hello World!----git-test02");
         System.out.println("Hello World!----git-test02");
-        System.out.println("bbbbbbbbbb");
-
     }
 }
